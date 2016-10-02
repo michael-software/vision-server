4d21331e83c8498f7b99d76469386d929bcfcfac
Michael Braun <michaelsoftware1997@gmail.com>
2016-10-02 16:21:37 +0200

    Updated plg_ts3viewer

--- plugins/plg_ts3viewer/controller/Ts3Viewer.php
+++ plugins/plg_ts3viewer/controller/Ts3Viewer.php
@@ -7,20 +7,30 @@ class Ts3Viewer {
 	var $fp;
 	
  	function connect() {
-		header("Content-Type: text/plain");
+		$this->fp = @fsockopen($this->tshost, $this->tsqueryport, $errno, $errstr, 5);
 		
-		$this->fp = fsockopen($this->tshost, $this->tsqueryport, $errno, $errstr, 5);
-		fgets($this->fp);
-		fgets($this->fp);
+		if(!empty($this->fp)) {
+			fgets($this->fp);
+			fgets($this->fp);
+			
+			fputs($this->fp,"login client_login_name=serveradmin client_login_password=".$this->tsquerypw."\n");
+			fgets($this->fp);
+			
+			fputs($this->fp,"use sid=1\n");
+			fgets($this->fp);
+			
+			return true;
+		}
 		
-		fputs($this->fp,"login client_login_name=serveradmin client_login_password=".$this->tsquerypw."\n");
-		fgets($this->fp);
-		
-		fputs($this->fp,"use sid=1\n");
-		fgets($this->fp);
-		
-		return true;
+		return false;
  	}
+	
+	function setNickname($pNickname) {
+		$pNickname = $this->getTsString($pNickname);
+		
+		fputs($this->fp,"clientupdate client_nickname=" . $pNickname . "\n");
+		fgets($this->fp);
+	}
 	
 	function clientList() {
 		if(!empty($this->fp)) {
@@ -33,21 +43,53 @@ class Ts3Viewer {
 		}
 	}
 	
-	function setNickname($pNickname) {
-		$pNickname = $this->getTsString($pNickname);
-		
-		fputs($this->fp,"clientupdate client_nickname=" . $pNickname . "\n");
-		fgets($this->fp);
-	}
-	
-	function channelList() {
+	function clientDbList() {
 		if(!empty($this->fp)) {
-			fputs($this->fp,"channellist\n");
+			fputs($this->fp,"clientdblist\n");
 			
 			$clientList = $this->getLine();
 			$statusLine = $this->getLine();
 			
+			return $this->parseInfo($clientList);
+		}
+	}
+	
+	function customInfo($client_database_id) {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"custominfo cldbid=" . $client_database_id . "\n");
+			
+			$clientList = $this->getLine();
+			
+			echo "TEST" . PHP_EOL;
+			
 			return $clientList;
+			
+			if(!$this->isError($clientList)) {
+				$statusLine = $this->getLine();
+				
+				return $this->parseInfo($clientList);
+			}
+		}
+	}
+	
+	function clientListInfoByDatabaseId() {
+		if(!empty($this->fp)) {
+			$clients = $this->clientList();
+			
+			$list = null;
+			
+			if(!empty($clients) && is_array($clients))
+			foreach($clients as $client) {
+				$client_database_id = $client['client_database_id'];
+				$client_id = $client['clid'];
+				$client_info = $this->clientInfo($client_id);
+				
+				if(empty($list[$client_database_id]) || $list[$client_database_id]['client_lastconnected'] > $client_info['client_lastconnected']) {
+					$list[$client_database_id] = $client_info;
+				}
+			}
+			
+			return $list;
 		}
 	}
 	
@@ -68,10 +110,128 @@ class Ts3Viewer {
 			$return[$i]['cid']                = $cid[1];
 			$return[$i]['client_database_id'] = $client_database_id[1];
 			$return[$i]['client_nickname']    = str_replace('\s', ' ', $client_nickname[1]);
-			$return[$i]['client_type']        = $client_type[1];
+			$return[$i]['client_type']        = str_replace('\s', ' ', $client_type[1]);
 		}
 		
 		return $return;
+	}
+	
+	function channelList($sorted = false) {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"channellist\n");
+			
+			$clientList = $this->getLine();
+			$statusLine = $this->getLine();
+			
+			return $this->parseChannelList($clientList, $sorted);
+		}
+	}
+	
+	function parseChannelList($pListString, $pSorted) {
+		$return = array();
+		$list = explode('|', $pListString);
+		
+		for($i = 0; $i < count($list); $i++) {
+			$client_info = explode(' ', $list[$i]);
+			
+			$cid                = explode('=', $client_info[0]);
+			$pid                = explode('=', $client_info[1]);
+			$channel_order      = explode('=', $client_info[2]);
+			$channel_name       = $this->decode(explode('=', $client_info[3]));
+			$total_clients      = explode('=', $client_info[4]);
+			
+			$channel_needed_subscribe_power = explode('=', $client_info[5]);
+			
+			if($pSorted) {
+				$cid = $cid[1];
+				$return[$cid]['pid']                = $pid[1];
+				$return[$cid]['channel_order']      = $channel_order[1];
+				$return[$cid]['channel_name']       = str_replace('\s', ' ', $channel_name[1]);
+				$return[$cid]['total_clients']      = $total_clients[1];
+				
+				$return[$cid]['channel_needed_subscribe_power'] = $channel_needed_subscribe_power[1];
+			} else {
+				$return[$i]['cid']                = $cid[1];
+				$return[$i]['pid']                = $pid[1];
+				$return[$i]['channel_order']      = $channel_order[1];
+				$return[$i]['channel_name']       = str_replace('\s', ' ', $channel_name[1]);
+				$return[$i]['total_clients']      = $total_clients[1];
+				
+				$return[$i]['channel_needed_subscribe_power'] = $channel_needed_subscribe_power[1];
+			}
+		}
+		
+		return $return;
+	}
+	
+	function serverInfo() {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"serverinfo\n");
+			
+			$clientList = $this->getLine();
+			$statusLine = $this->getLine();
+			
+			return $this->parseSingleInfo($clientList);
+		}
+	}
+	
+	function serverGroupsByClientId($client_database_id) {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"servergroupsbyclientid cldbid=" . $client_database_id . "\n");
+			
+			$clientList = $this->getLine();
+			if(!$this->isError($clientList)) {
+				$statusLine = $this->getLine();
+				
+				return $this->parseInfo($clientList);
+			}
+		}
+	}
+	
+	function parseInfo($pListString) {
+		$infos = explode('|', $pListString);
+		
+		$array = null;
+		foreach ($infos as $info) {
+			$array[] = $this->parseSingleInfo($info);
+		}
+		
+		return $array;
+	}
+	
+	function parseSingleInfo($pListString) {
+		$infos = explode(' ', $pListString);
+		
+		$array = null;
+		foreach($infos as $info) {
+			$nameValue = explode('=', $info);
+			
+			$name  = $nameValue[0];
+			$name  = str_replace(" ", "", $name);
+			$name  = str_replace(array("\r\n","\r","\n"), "", $name);
+			
+			unset($nameValue[0]);
+			$value = implode('=', $nameValue);
+			
+			$array[$name] = $this->decode($value);
+		}
+		
+		return $array;
+	}
+	
+	function channelClientList($cid) {
+		$clients = $this->clientList();
+		
+		$array = null;
+		
+		if(!empty($clients))
+		foreach($clients as $client) {
+			if($client['cid'] == $cid) {
+				$array[] = $client;
+			}
+		}
+		
+		return $array;
 	}
 	
 	function getLine() {
@@ -84,6 +244,15 @@ class Ts3Viewer {
 			$pMessage = $this->getTsString($pMessage);
 			
 			fputs($this->fp,"sendtextmessage targetmode=1 target=" . $pTarget. " msg=" . $pMessage . "\n");
+			$this->getLine();
+		}
+	}
+	
+	function writeBroadcast($pMessage) {
+		if(!empty($this->fp)) {
+			$pMessage = $this->getTsString($pMessage);
+			
+			fputs($this->fp,"sendtextmessage targetmode=3 target=1 msg=" . $pMessage . "\n");
 			$this->getLine();
 		}
 	}
@@ -102,10 +271,102 @@ class Ts3Viewer {
 		}
 	}
 	
+	function moveClient($clid, $cid) {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"clientmove clid=" . $clid. " cid=" . $cid . "\n");
+			$this->getLine();
+		}
+	}
+	
+	function clientInfo($clid) {
+		if(!empty($this->fp)) {
+			fputs($this->fp,"clientinfo clid=" . $clid. "\n");
+			
+			$clientinfo = $this->getLine();
+			
+			if(!$this->isError($clientinfo)) {
+				$statusLine = $this->getLine();
+				
+				return $this->parseSingleInfo($clientinfo);
+			}
+		}
+		
+		return null;
+	}
+	
+	function isError($call) {
+		if(empty($call)) {
+			return false;
+		} else if(strpos($call, "error") !== FALSE && strpos($call, "id=0") !== FALSE) {
+			return true;
+		} else if(strpos($call, "error") === FALSE) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	function parseClientInfo($pListString) {
+		$return = array();
+		$list = explode('|', $pListString);
+		
+		$returnArray = null;
+		for($i = 0; $i < count($list); $i++) {
+			$infos = explode(' ', $list[$i]);
+			
+			$array = null;
+			foreach($infos as $info) {
+				$pair = explode('=', $info);
+				
+				$name = $pair[0];
+				$value = $pair[1];
+				
+				$array[$name] = $value;
+			}
+			
+			if(!empty($array))
+			$returnArray[] = $array;
+		}
+		
+		return $returnArray;
+	}
+	
 	function getTsString($pString) {
 		$pString = str_replace(" ", "\s", $pString);
+		$pString = str_replace("/", "\/", $pString);
 		
 		return $pString;
+	}
+	
+	function decode($pString) {
+		$pString = str_replace("\s", " ", $pString);
+		$pString = str_replace("\/", "/", $pString);
+		
+		return $pString;
+	}
+	
+	function getFileList($cid, $path='/') {
+		if(!empty($this->fp)) {
+			$path = $this->getTsString($path);
+			fputs($this->fp,"ftgetfilelist cid=" . $cid . " cpw= path=" . $path . "\n");
+			
+			$clientList = $this->getLine();
+			$statusLine = $this->getLine();
+			
+			return $this->parseInfo($clientList);
+		}
+	}
+	
+	function initDownload($cid, $path='/') {
+		if(!empty($this->fp)) {
+			$path = $this->getTsString($path);
+			fputs($this->fp,"ftinitdownload clientftfid=1 name=" . $path . " cid=" . $cid . " cpw= seekpos=0\n");
+			
+			$clientList = $this->getLine();
+			$statusLine = $this->getLine();
+			
+			return $this->parseInfo($clientList);
+		}
 	}
 	
 	function close() {
--- plugins/plg_ts3viewer/view/kick.php
+++ plugins/plg_ts3viewer/view/kick.php
@@ -3,12 +3,13 @@ include $pluginManager->getController('Ts3Viewer');
  
 $ts3viewer = new Ts3Viewer();
 if($ts3viewer->connect()) {
-	$ts3viewer->setNickname("Vision");
+	$ts3viewer->setNickname("Vision (Webinterface: " . $loginManager->getUsername() . ")");
 	
 	$command = $pluginManager->getCommand(0);
 	
 	if(!empty($command)) {
-		$ts3viewer->kick($command, "Test");
+		$logManager->addLog($loginManager->getUsername() . " hat den Benutzer mit der Nummer " . $command . " aus dem Channel gekickt.");
+		$ts3viewer->kick($command, "Sie wurden aus dem Channel gekickt");
 		die('{"redirect":["' . $pluginManager->getPluginName() . '", "user", ""]}');
 	}
 	
