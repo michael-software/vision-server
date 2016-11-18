8ea1303877ad525a0956da561aa758f0d1822296
Michael Braun <michaelsoftware1997@gmail.com>
2016-11-18 18:04:00 +0100

    Allowed to use pluginManager in Async-Call

--- includes/AsyncManager.php
+++ includes/AsyncManager.php
@@ -1,15 +1,21 @@
 <?php
 
+require_once(dirname(__FILE__) . '/PluginManager.php');
+
 class AsyncManager {
 	private $pluginDir;
+	private $plugin;
 	
 	function __construct($user, $plugin) {
 		$this->pluginDir = dirname(dirname(__FILE__)) . '/plugins/' . $plugin . '/';
+		$this->plugin = $plugin;
 	}
 	
 	function triggerAction($action, $value) {
+		$pluginManager =  new PluginManager($this->plugin);
+
 		$actionFile = $this->pluginDir . 'listener/async.php';
-		
+
 		if(is_dir($this->pluginDir) && is_file($actionFile)) {
 			include $actionFile;
 		}
