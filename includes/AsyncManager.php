9b7e4e58c72c5b605d41c76b2c7eb341c04cdb52
Michael Braun <michaelsoftware1997@gmail.com>
2016-12-31 00:35:50 +0100

    Changed AsyncManager to be called over API

--- includes/AsyncManager.php
+++ includes/AsyncManager.php
@@ -1,22 +1,46 @@
 <?php
 
 require_once(dirname(__FILE__) . '/PluginManager.php');
-require_once(dirname(__FILE__) . '/LoginManagerAsync.php');
+
+if(constant('WEBSOCKET') == 1)
+    require_once(dirname(__FILE__) . '/LoginManagerAsync.php');
+else
+    require_once(dirname(__FILE__) . '/LoginManager.php');
 
 class AsyncManager {
 	private $pluginDir;
 	private $plugin;
     private $user;
 	
-	function __construct($user, $plugin) {
-		$this->pluginDir = dirname(dirname(__FILE__)) . '/plugins/' . $plugin . '/';
-		$this->plugin = $plugin;
-        $this->user = $user;
+	function __construct() {
+        $a = func_get_args();
+        $i = func_num_args();
+
+        if (method_exists($this, $f='__construct'.$i)) {
+            call_user_func_array(array($this,$f), $a);
+        }
 	}
+
+    function __construct1($plugin) {
+        $this->pluginDir = dirname(dirname(__FILE__)) . '/plugins/' . $plugin . '/';
+        $this->plugin = $plugin;
+    }
+
+    function __construct2($user, $plugin) {
+        $this->pluginDir = dirname(dirname(__FILE__)) . '/plugins/' . $plugin . '/';
+        $this->plugin = $plugin;
+        $this->user = $user;
+    }
+
+
 	
 	function triggerAction($action, $value) {
 		$pluginManager =  new PluginManager($this->plugin);
-        $loginManager = new LoginManagerAsync($this->user);
+
+        if(constant('WEBSOCKET') == 1)
+            $loginManager = new LoginManagerAsync($this->user);
+        else
+            global $loginManager;
 
 		$actionFile = $this->pluginDir . 'listener/async.php';
 
